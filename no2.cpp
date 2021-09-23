46d7843d998e1be3eac9a755dc6d010b254eb4ed
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-09-23 23:54:53 +0700

    new ugm

---
+++ no2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/* Jangan Lupa Follow IG : Rizkkyab
+
+    Belajar yang giat biar kaya dosen
+ */
+int main()
+{
+	int A ;
+	int B ;
+
+	cout << "Data Pertama Sebelum Pertukaran \n";
+
+    cout <<"Data A : ";
+    cin >>A;
+    cout <<"Data B : ";
+    cin >>B;
+
+	cout << "\n=====================================\n";
+	cout << "\nData Setelah Dituker \n";
+
+	cout << "\nData A = " << B;
+	cout << "\nData B = " << A;
+	cout <<"\n";
+	cout << "\n=====================================";
+}
